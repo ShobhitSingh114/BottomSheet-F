044e9d8dc5d9831be41e43f66eeb707651a86a59
Shobhit <shobhitsingh1149201@gmail.com>
2024-02-24 19:43:11 +0530

    completed

--- app/src/main/java/com/example/bottomsheet_f/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheet_f/MainActivity.kt
@@ -3,12 +3,25 @@ package com.example.bottomsheet_f
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bottomsheet_f.ui.theme.BottomSheetFTheme
 
@@ -22,11 +35,51 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    BottomSheet()
                 }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheet() {
+
+    val sheetState = rememberModalBottomSheetState()
+    // rememberSaveAble for screenRotation
+//    var isSheetOpen by rememberSaveable {
+//        mutableStateOf(false)
+//    }
+    var isSheetOpen by remember {
+        mutableStateOf(false)
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = {
+                isSheetOpen = true
+            }
+        ) {
+            Text(text = "Open Sheet")
+        }
+    }
+
+    // if sheetOpen then only we wanna see the ModalBottomSheet
+    if (isSheetOpen) {
+        ModalBottomSheet(
+            // sheetState = used for to control the visibility of the bottom sheet
+            sheetState = sheetState,
+            // onDismissRequest = this is called when we swipe it away or tap outside of the bottom sheet
+            onDismissRequest = { isSheetOpen = false }
+        ) {
+            Image(painter = painterResource(id = R.drawable.kermit), contentDescription = "kermit")
+        }
+    }
+
 }
 
 
